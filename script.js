1cb0c9c73af3c65a42db03b189ef526b5b3054e4
Svet <stsupryk@gmail.com>
2019-07-12 15:23:12 +0300

    implement validation for phone number

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 
     Scrolling();
     ChoosePrice();
-
-
+    sendPhone();
 
 
     //////function for moving left after scroll
@@ -67,40 +66,13 @@
         });
     }
 
-    function Tabs() {
-        var works = document.querySelector(".works");
-        var tab = works.querySelectorAll('[data-item]');
-        var cont = works.querySelector('.works__content');
-        var tabLength = tab.length;
-        var tabcontent = cont.querySelectorAll('.works__tabcontent');
-        tab.forEach(() => {
-            tab[i].addEventListener("click", show);
-        });
+    //////// send form with validation input 
 
-        function show() {
-            tabcontent.forEach(function (item, i, arr) {
-                tabcontent[i].classList.remove("show");
-            });
-            var c = 'data-item =' + '"' + this.dataset.item + '"';
-            cont.querySelector("[" + c + "]").classList.add("show");
-        };
-    }
-
-
-    //////// validation form 
-
-    function jsForm() {
+    function sendPhone() {
         let form = document.querySelector('.js-form');
-        if (!form) return console.log('Форма не найдена');
         form.setAttribute('novalidate', 'true');
-        let inputs = form.querySelectorAll('.form__input');
+        let input = form.querySelector('.js-input');
         let isValid = true;
-        let errorClass = 'form__error';
-        let errors = {
-            default: errorClass,
-            required: errorClass + '--required',
-            pattern: errorClass + '--pattern'
-        }
 
         form.addEventListener('submit', function (e) {
             e.preventDefault();
@@ -113,23 +85,17 @@
         });
 
         function setToDefaultStyles() {
-            inputs.forEach(element => {
-                element.classList.remove(errors.default, errors.required, errors.pattern);
-            });
+            input.classList.remove("error");
         }
 
         function validateInputs() {
-            inputs.forEach(element => {
-                isValid = checkOnRequired(element) && isValid; // true or false
-            });
-
+            isValid = checkOnRequired(input) && isValid;
         };
 
         function checkOnRequired(input) {
             if (input.hasAttribute('required')) {
                 if (input.value.trim() === '') {
-                    input.classList.add(errors.required);
-
+                    input.classList.add("error");
                     return false;
                 } else {
                     return true;
@@ -141,99 +107,30 @@
 
         function submitForm() {
             if (!isValid) return console.log('NOT VALID');
-            let femail = form.querySelector(".js-email");
 
-            if (validateEmail(femail.value)) {
-                console.log("ok");
+            if (validate(input.value)) {
+                alert("Введите номер телефона для заявки");
+                input.classList.add("sent");
                 isValid = true;
             } else {
-                swal({
-                    type: 'error',
-                    title: 'Oops...',
-                    text: 'Please enter correct email!'
-                });
-                //alert("Please enter correct email");
+                alert("Введите корректный номер телефона");
                 isValid = false;
-                femail.classList.add(errors.pattern);
-
+                input.classList.add("error");
             }
 
-            function validateEmail(email) {
-                let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return re.test(String(email).toLowerCase());
+            function validate(value) {
+                let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+                return re.test(value);
             }
             // AJAX
-
 
             // Success messages
             if (isValid) {
                 //sendForm();
-
-                swal(
-                    'Good job!',
-                    'You log in!',
-                    'success'
-                )
-                form.querySelector('.form__btn').disabled = true;
+                alert("Спасибо!");
             } else {
                 form.reset();
-
             }
         }
-    }
-
-
-    /////Gsap
-
-    function Tween() {
-        let tl = new TimelineMax();
-        tl
-            .from('#left', 2, {
-                x: '-300%',
-                ease: Bounce.easeOut
-            }, delay = 1)
-            .from('#right', 2, {
-                x: '300%',
-                ease: Bounce.easeOut
-            }, "-=2")
-            .from('.wellcome__btn', .5, {
-                borderRadius: "30px"
-            })
-            .from('.logo__h1', 1, {
-                y: '-100',
-                opacity: 0
-            }, "-=0.5")
-            .staggerFrom(".nav__list", .5, {
-                y: '-50',
-                opacity: 0
-            }, 0.1, "-=0.25")
-    }
-
-    function scrollMagic() {
-        // init controller
-        var controller = new ScrollMagic.Controller();
-
-        var tween = TweenMax.from('#about', 1, {
-            y: 500,
-            opacity: 0
-        });
-        var tween1 = TweenMax.from('#contacts', 1, {
-            y: 500,
-            opacity: 0
-        });
-        // create a scene
-
-        var scene = new ScrollMagic.Scene({
-                triggerElement: "#servises",
-                offset: 400
-            })
-            .setTween(tween)
-            .addTo(controller);
-        var scene2 = new ScrollMagic.Scene({
-                triggerElement: "#clients",
-                offset: 300
-            })
-            .setTween(tween1)
-            .addTo(controller);
     }
 })();
